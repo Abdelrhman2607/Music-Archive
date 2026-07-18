836fe9c978c2fb96c8911c394180bd1b9adae7cc
Abdelrahman ElSayed <139968165+Abdelrhman2607@users.noreply.github.com>
2026-07-18 18:44:24 +0300

    Database intializaion script

---
+++ backend/db_init.sql
@@ -0,0 +1,68 @@
+/* Windows command to run in psql shell: \i 'D:/Music-Archive/backend/db_init.sql' */
+
+DROP DATABASE IF EXISTS music_archive;
+CREATE DATABASE music_archive;
+\c music_archive;
+
+DROP TABLE IF EXISTS music_entries;
+CREATE TABLE music_entries(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL DEFAULT 'Unnamed entry',
+    date_added DATE NOT NULL DEFAULT CURRENT_DATE,
+    description TEXT
+);
+
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE DEFAULT 'Unnamed tag'
+);
+
+DROP TABLE IF EXISTS tag_entries;
+CREATE TABLE tag_entries(
+    tag_id INT NOT NULL REFERENCES tags(id)
+    ON DELETE CASCADE,
+
+    entry_id INT NOT NULL REFERENCES music_entries(id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (tag_id, entry_id)
+);
+
+DROP TABLE IF EXISTS artists;
+CREATE TABLE artists(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE DEFAULT 'Unnamed Artist'
+);
+
+DROP TABLE IF EXISTS artist_entries;
+CREATE TABLE Artistentries(
+    artist_id INT NOT NULL REFERENCES artists(id)
+    ON DELETE CASCADE,
+
+    entry_id INT NOT NULL REFERENCES music_entries(id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (artist_id, entry_id)
+);
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE DEFAULT 'Unnamed category',
+    parent_id INT REFERENCES categories(id)
+);
+
+DROP TABLE IF EXISTS category_entries;
+CREATE TABLE category_entries(
+    category_id INT NOT NULL REFERENCES categories(id)
+    ON DELETE RESTRICT,
+
+    entry_id INT NOT NULL REFERENCES music_entries(id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (category_id, entry_id)
+);
+
+
+

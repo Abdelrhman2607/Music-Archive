94f9873a9f74b941ffef609c8adca438c4dd7717
Abdelrahman ElSayed <139968165+Abdelrhman2607@users.noreply.github.com>
2026-07-21 06:48:29 +0300

    dummy backend

---
+++ app/components/EntryDisplayGrid/entryDisplayGrid.tsx
@@ -0,0 +1,13 @@
+
+import styles from './entryDisplayGrid.module.css';
+
+export default function EntryDisplayGrid(){
+    return(
+        <div className={styles.entryGrid}>
+            <p>1</p>
+            <p>4</p>
+            <p>2</p>
+            <p>3</p>
+        </div>
+    );
+}

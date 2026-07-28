abeafcd9a4ea8ca0a2ff371c2475f3c4bc0af631
Abdelrahman ElSayed <139968165+Abdelrhman2607@users.noreply.github.com>
2026-07-28 03:55:55 +0300

    Website Outfit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
+import { Outfit } from 'next/font/google'
 import "./globals.css";
 
 import Sidebar from './components/Sidebar/sidebar';
+
+const outfit = Outfit({
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Music Archive",
@@ -11,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children, }: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body>
+      <body className={outfit.className}>
         <Sidebar />
         <div className="ml-[12vw]">
           {children}

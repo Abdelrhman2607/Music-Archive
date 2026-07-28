8295c1057f0b36baad6f792496a25d961810d2a0
Abdelrahman ElSayed <139968165+Abdelrhman2607@users.noreply.github.com>
2026-07-28 04:06:25 +0300

    Edit page WIP

--- app/(entry)/edit/[EntryID]/page.tsx
+++ app/(entry)/edit/[EntryID]/page.tsx
@@ -1,9 +1,16 @@
+'use client';
 
+import { useParams } from 'next/navigation';
+import styles from './page.module.css';
+import EntryForm from '@/app/components/EntryForm/entryForm';
 
-export default function EditEntryPage(){
+export default function newEntryPage(){
+    const params = useParams<{EntryID: string}>();
+    console.log(params.EntryID)
     return(
     <main>
-        editEntry
+        <h1 className={styles.pageHeader}>Edit Entry: <span style={{ fontWeight: '600'}}>{params.EntryID}</span></h1>
+        <EntryForm />
     </main>
     );
 }
